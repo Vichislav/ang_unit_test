3d1790320215e135095fd185206cc69c433f8c49
Svalyan <you@example.com>
2022-07-14 15:12:03 +0600

    new component and new dependency

---
+++ src/app/async-example/async-example.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-async-example',
+  templateUrl: './async-example.component.html',
+  styleUrls: ['./async-example.component.css']
+})
+export class AsyncExampleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
